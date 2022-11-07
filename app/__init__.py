1f90bf30485fde45634da3c50fb1b83b8c2939d1
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-11-07 18:36:22 -0500

    Started database and login work

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,24 @@ time spent: 2hrs
 
 
 from flask import Flask, render_template, request, session, redirect, url_for
+import sqlite3
+import csv
 
-#the conventional way:
-#from flask import Flask, render_template, request
+
+'''
+
+'''
+
+DB_FILE = "website_information.db"
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS account_information(username TEXT UNIQUE, password TEXT)")
+c.execute("""CREATE TABLE IF NOT EXISTS story_list(storyname TEXT UNIQUE, tag TEXT, contributors TEXT, fullstory TEXT,
+latestupdate TEXT, totalupdates INTEGER, latestcontributor TEXT, latestupdatetime TEXT)""")
+
 
 app = Flask(__name__)    #create Flask object
-
-
-'''
-
-'''
 
 app.secret_key = '77ad3ef4fbaf3d0cd0db350b92373f8aef6ec1843bffbef0626398d52be358cf'
 
@@ -35,13 +43,29 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login(): ## Maybe more methods will work in the /login root
     if request.method == 'POST':
-        if request.form['username'] in userpass.keys():
-            if userpass[request.form['username']] == request.form['password']:
+        namepasslist = c.execute("SELECT * from account_information;").fetchall()
+        names = [index[0] for index in namepasslist] #creates a list of just the names
+        if request.form['username'] in names:
+            if request.form['password'] == c.execute("SELECT password from account_information WHERE username = '" + request.form['username'] + "';").fetchone()[0]:
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
-            return render_template('login.html', error = "Password is incorrect")
-        return render_template('login.html', error = "Username does not exist")
-    return render_template('login.html')
+            return render_template('login.html', use = "LOGIN", error = "Password is incorrect")
+        return render_template('login.html', use = "LOGIN", error = "Username does not exist")
+    return render_template('login.html', use = "LOGIN")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        namepasslist = c.execute("SELECT * from account_information;").fetchall()
+        names = [index[0] for index in namepasslist] #creates a list of just the names
+        if request.form['username'] not in names:
+            if "|" not in request.form['password'] and len(request.form['password']) > 0:
+                c.execute("INSERT INTO account_information VALUES ('" + request.form['username'] + "', '" + request.form['password'] + "');")
+                return redirect(url_for('login'))
+            return render_template('login.html', use = "REGISTER", error = "Password contains invalid character '|' or is too short")
+        return render_template('login.html', use = "REGISTER", error = "Username already exists")            
+    return render_template('login.html', use = "REGISTER") # use login.html for register page, but change text on site
+#     return redirect(url_for('login'))
 
 @app.route("/home", methods=['GET', 'POST'])
 def home():
